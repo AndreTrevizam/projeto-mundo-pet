6ef4fc49203a51131a7c93cc589e416de11efa77
AndreTrevizam <andretrevizam264@gmail.com>
2025-01-29 14:35:33 -0300

    Configurações dos pacotes

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict"
+
+// CSS
+import "./styles/form.css"
+import "./styles/global.css"
+import "./styles/header.css"
+import "./styles/main.css"
+import "./styles/modal.css"
+import "./styles/utility.css"
